c67cf06dc8cad4a596d529d14cc7b299fe1d3795
6510615138 <r.nakinton@gmail.com>
2025-05-12 14:34:14 +0700

    change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tuquest"
+        applicationId = "tuquest"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
